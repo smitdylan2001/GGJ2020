c27b0bbb7991561251554c883ff1adc1f92293ff
Dylan Smit <dylan.smit01@gmail.com>
2021-01-29 20:01:51 +0100

    Added and removed rope asset
    Still some leftovers

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlightController : MonoBehaviour
+public class CharacterController : MonoBehaviour
 {
     private Rigidbody _rb;
-    private     
+    private 
 
     // Start is called before the first frame update
     void Start()
